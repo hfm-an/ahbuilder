432ada67a86702d4b90d738d4f2bed8f8dc0cab3
hfm-an <hfm_an@163.com>
2018-11-04 18:14:04 +0800

    init src

---
+++ src/index.js
@@ -0,0 +1,1 @@
+const parseFile = requier('./parseFile')
---
+++ src/parseFile.js
@@ -0,0 +1,6 @@
+const { readFileSync } = require('fs')
+const traverse = require('babel-traverse')
+
+module.exports = function (path) {
+
+}
